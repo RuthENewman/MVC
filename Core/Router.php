3fa0d1f0b8047b587d96c108e9c60cd8eb02ebc1
Ruth Newman <ruthnewman@gmail.com>
2019-11-26 00:15:49 +0000

    added first controller

--- Core/Router.php
+++ Core/Router.php
@@ -81,6 +81,54 @@ class Router
         return $this->params;
     }
 
+    /**
+     * 
+     */
+    public function dispatch($url)
+    {
+        if ($this->match($url))
+        {
+            $controller = $this->params['controller'];
+            $controller = $this->capitalise($controller);
+
+            if (class_exists($controller)) {
+                $controllerObject = new $controller();
+                $action = $this->params['action'];
+                $action = $this->camelCase($action);
+
+                if (is_callable([$controllerObject, $action])) {
+                    $controllerObject->$action();
+                } else {
+                    echo "The $action method in the $controller controller was not found";
+                }
+            } else {
+                echo "The $controller controller was not found.";
+            }
+        } else {
+            echo "No route matched";
+        }
+    }
+
+    /**
+     * Converts a kebab case string to capitalise it
+     * @param string $string
+     * @return string
+     */
+    protected function capitalise($string)
+    {
+        return str_replace(' ', '', ucwords(str_replace('-', ' ', $string)));
+    }
+
+    /**
+     * Converts a kebab case string to camelCase
+     * @param string $string
+     * @return string
+     */
+    protected function camelCase($string)
+    {
+        return lcfirst($this->capitalise($string));
+    }
+
 }
 
 
