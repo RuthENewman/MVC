2820d0677a91590bbc9b1f502d0daa100889ede2
Ruth Newman <ruthnewman@gmail.com>
2019-11-29 00:34:04 +0000

    first view

--- App/Controllers/Home.php
+++ App/Controllers/Home.php
@@ -2,11 +2,24 @@
 
 namespace App\Controllers;
 
+use \Core\View;
+
 class Home extends \Core\Controller
 {
+    protected function before()
+    {
+        //echo "(before) ";
+        //return false;
+    }
+
+    protected function after()
+    {
+        //echo " (after)";
+    }
+
     public function indexAction()
     {
-        echo "Hello from the index action in the Home controller";
+        View::render('Home/index.php');
     }
 
 
--- App/Controllers/Posts.php
+++ App/Controllers/Posts.php
@@ -4,19 +4,19 @@ namespace App\Controllers;
 
 class Posts extends \Core\Controller
 {
-    public function index()
+    public function indexAction()
     {
         echo "Hello from the index action in the Posts controller";
         // echo '<p>Query string parameters: <pre>' . 
         //       htmlspecialchars(print_r($_GET, true)) . '</pre></p>';
     }
 
-    public function addNew()
+    public function addNewAction()
     {
         echo "Hello from the addNew action";
     }
 
-    public function edit()
+    public function editAction()
     {
         echo "Hello from the edit action in the Posts controller";
         echo '<p>Route parameters: <pre>' . 
